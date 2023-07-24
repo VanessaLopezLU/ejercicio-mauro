942e7a901deddea0cfbe5ecce6130e52f4a34b1e
Vanessa Lopez <leylopez32@gmail.com>
2023-07-24 16:08:51 -0500

    ejercicio peso y sexo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,53 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post()
+  recibirBody() {
+    return this.appService.obtenerPost();
+  }
+
+  @Post('/param/:id')
+  recibirParametro(@Param('id') id: string) {
+    return this.appService.recibirParametros(id);
+  }
+  @Post('/crearuser')
+  recibirbody(@Body() cuerpo: {}) {
+    return this.appService.recibirbody(cuerpo)
+  }
+  @Post('/suma')
+  sumar(@Body() cuerpo: {}) {
+    return this.appService.sumar(cuerpo);
+  }
+  @Post('/restar')
+  restar(@Body() cuerpo: {}) {
+    return this.appService.restar(cuerpo);
+  }
+  @Post('/multiplicar')
+  multiplicar(@Body() cuerpo: {}) {
+    return this.appService.multiplicar(cuerpo);
+  }
+  @Post('/division')
+  division(@Body() cuerpo: {}) {
+    return this.appService.division(cuerpo);
+  }
+  @Post('/peso')
+  peso(@Body() cuerpo:{}){
+    return this.appService.peso(cuerpo);
+
+  }
+  @Post('/sexo')
+  sexo(@Body() genero:{}){
+    return this.appService.sexo(genero);
+
+  }
+  
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,54 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  obtenerPost(){
+    return "llego el post"
+  }
+  recibirParametros(id:string){
+    return id;
+  }
+  recibirbody(cuerpo:{}){
+    return cuerpo;
+  }
+  sumar(cuerpo){
+    return cuerpo.numero + cuerpo.otronumero 
+  }
+  restar(cuerpo){
+    return cuerpo.numero - cuerpo.otronumero 
+  }
+  multiplicar(cuerpo){
+    return cuerpo.numero * cuerpo.otronumero 
+  }
+  division(cuerpo){
+    return cuerpo.numero / cuerpo.otronumero 
+  }
+  peso(cuerpo){
+    
+    let p= cuerpo.pesoKg/( cuerpo.alturaMtr * cuerpo.alturaMtr);
+    if(p<0){
+      return 'Ingrese los datos correctamente'
+    }
+    if (p<18.5){
+      return 'Bajo de peso';
+    } else if( p>= 18 &&  p < 25){
+      return 'Peso Normal';
+    }else if(p>=25) {
+      return 'Sobre Peso'
+    }
+    if(cuerpo.pesoKg <=0 || cuerpo.alturaMtr<=0){
+      return 'ingrese Datos Correctos'
+
+    }
+  }
+  sexo(genero){
+    
+    if (genero.op === 'M') {
+      return 'masculino';
+    } else if (genero.op === 'F') {
+      return 'femenino';
+    } else {
+      return 'Opción inválida. Debe ser "masculino" o "femenino".';
+    }
+
+  }
 }
